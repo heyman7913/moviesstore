ec7cfdd9df9de6e23b90ddc0ce3cca48ca6e95b8
heyman7913 <chitkarahimansh5@gmail.com>
2025-09-16 16:14:22 -0400

    Final Commit

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Movie, Review, MovieRequest
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from .models import Movie, Review, MovieRequest
 
 
 # Create your views here.
